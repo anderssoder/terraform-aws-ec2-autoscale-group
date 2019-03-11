59a91294ae58dda6a3373de04f189915bf2b2091
Anders Soder <anders.soder@kindredgroup.com>
2019-03-11 16:16:58 +0100

    fix cf output issue, workaround test

--- autoscaling.tf
+++ autoscaling.tf
@@ -12,7 +12,7 @@ resource "aws_autoscaling_policy" "scale_up" {
   policy_type            = "${var.scale_up_policy_type}"
   cooldown               = "${var.scale_up_cooldown_seconds}"
   autoscaling_group_name = "${local.asg_name}"
-  depends_on             = "${aws_cloudformation_stack.default}"
+  depends_on             = "aws_cloudformation_stack.default"
 }
 
 resource "aws_autoscaling_policy" "scale_down" {
@@ -23,7 +23,7 @@ resource "aws_autoscaling_policy" "scale_down" {
   policy_type            = "${var.scale_down_policy_type}"
   cooldown               = "${var.scale_down_cooldown_seconds}"
   autoscaling_group_name = "${local.asg_name}"
-  depends_on             = "${aws_cloudformation_stack.default}"
+  depends_on             = "aws_cloudformation_stack.default"
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_high" {
@@ -43,7 +43,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high" {
 
   alarm_description = "Scale up if CPU utilization is above ${var.cpu_utilization_high_threshold_percent} for ${var.cpu_utilization_high_period_seconds} seconds"
   alarm_actions     = ["${join("", aws_autoscaling_policy.scale_up.*.arn)}"]
-  depends_on        = "${aws_cloudformation_stack.default}"
+  depends_on        = "aws_cloudformation_stack.default"
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_low" {
@@ -63,5 +63,5 @@ resource "aws_cloudwatch_metric_alarm" "cpu_low" {
 
   alarm_description = "Scale down if the CPU utilization is below ${var.cpu_utilization_low_threshold_percent} for ${var.cpu_utilization_low_period_seconds} seconds"
   alarm_actions     = ["${join("", aws_autoscaling_policy.scale_down.*.arn)}"]
-  depends_on        = "${aws_cloudformation_stack.default}"
+  depends_on        = "aws_cloudformation_stack.default"
 }
